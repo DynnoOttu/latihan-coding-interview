e7f3a61ebac163ba12b9cd86e553494f73585d92
DynnoOttu <dynnoottu968@gmail.com>
2023-07-09 15:09:00 +0800

    latihan coding interview part 1

---
+++ part1/index.js
@@ -0,0 +1,78 @@
+// tulis fungsi yang mengambil 2 angka (a dan b) sebagai argument. jumlahkan a dan b. kembalikan hasilnya
+
+// function myFunction(a, b) {
+//     return a + b
+// }
+
+// console.log(myFunction(5, 5))
+// console.log(myFunction(1, 10))
+
+
+
+
+// tuliskan 2 fungsi yang mengambil 2 nilai, misalkan a dan b sebagai argument, kembalikan true jika kedua nilai sama dan bertipe data sama
+
+// function myFunction(a, b) {
+//     return a === b
+// }
+// console.log(myFunction(8, 8)) //true - nilai dan tipe data sama
+// console.log(myFunction(10, '10')) //false - tipe data tidak sama
+
+
+
+// tulis fungsi yang mengambil nilai sebagai argument. mengembalikan jenis nilai
+
+// function myFunction(a) {
+//     return typeof (a)
+// }
+
+// console.log(myFunction(1))
+// console.log(myFunction("ini string"))
+// console.log(myFunction(['ini array']))
+// console.log(myFunction(false))
+// console.log(myFunction({}))
+
+
+
+
+// tuliskan fungsi yang mengunakan string a dan angka sebagai n. mengembalikan karakter dari n ke a
+
+// function myFunction(a, n) {
+//     return a[n - 1]
+// }
+
+// console.log(myFunction('Hallo Dynno', 5))
+// console.log(myFunction('Hallo Dynno', 2))
+
+
+
+
+//tuliskan fungsi yang mengunakan string (a) sebagai argument. hapus 3 karakter pertama dari a. kembalikan hasilnya
+
+// function myFunction(a) {
+//     return a.slice(3)
+// }
+
+// console.log(myFunction('Yohanis'))
+
+
+
+
+// tuliskan fungsi yang mengunakan string sebagai argument. extrack 3 karakter terahkir sebagai string. kembalikan hasilnya
+
+// function myFunction(str) {
+//     return str.slice(-3)
+// }
+
+// console.log(myFunction('abcdef'))
+// console.log(myFunction('1234'))
+
+
+
+
+
+// function myFunction(a) {
+//     return a.indexOf('is')
+// }
+
+// console.log(myFunction('paris'))
